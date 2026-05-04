8769fb5747b2aca2a66fb5b4b36b1bd318501a1f
FelipeGuttemberg <fgos1@aluno.ifal.edu.br>
2026-05-04 16:49:10 -0300

    Testando

--- app/index.tsx
+++ app/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Link } from 'expo-router';
 
+import Voltar from '../components/buttons/Voltar';
+
 export default function Home() {
   return (
     <View style={estilos.view}>
       <Text style={estilos.text}>Bem Vindo</Text>
+      <Voltar />
     </View>
   );
 }
