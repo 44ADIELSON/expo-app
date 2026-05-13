aacaa30951be05c582d5a6030b3c9394f7b51c37
44ADIELSON <acs86@aluno.ifal.edu.br>
2026-05-13 16:54:31 -0300

    Ultimas alterações da ultima semana

--- project/components/user-area/mock/user-selection.tsx
+++ project/components/user-area/mock/user-selection.tsx
@@ -1,9 +1,7 @@
-import { Text, View, StyleSheet, FlatList, Pressable } from 'react-native';
-import {useState, useEffect} from 'react'
+import { Text, View, StyleSheet, FlatList, Pressable } from "react-native";
+import { useState, useEffect } from "react";
 
-import { useRouter } from 'expo-router';
-
-import UserCard from './user-card';
+import UserCard from "./user-card";
 
 type User = {
   nome: string;
@@ -11,27 +9,25 @@ type User = {
 };
 
 const SelecaoPerfil = () => {
-  const router = useRouter();
-
   const [dados, SetDados] = useState<User[]>([]);
 
   const getUsers = async () => {
     try {
       const response = await fetch(
-        'https://randomuser.me/api/?results=1&nat=br&gender=female'
+        "https://randomuser.me/api/?results=1&nat=br&gender=female",
       );
       const dados = await response.json();
       SetDados(dados);
 
       const metMAP = dados.results.map((dados: any) => {
         return {
-          nome: dados.name.first + ' ' + dados.name.last,
-          foto: dados.picture.large
+          nome: dados.name.first + " " + dados.name.last,
+          foto: dados.picture.large,
         };
       });
       SetDados(metMAP);
     } catch (error) {
-      console.log('Algo saiu mal', error);
+      console.log("Algo saiu mal", error);
     }
   };
 
@@ -41,18 +37,16 @@ const SelecaoPerfil = () => {
 
   return (
     <View style={estilos.view}>
-
       <View>
-          <FlatList
-            numColumns={3}
-            columnWrapperStyle={estilos.flatlistColum}
-            data={dados}
-            renderItem={({ item }: { item: User }) => (
-              <UserCard nome={item.nome} foto={item.foto} />
-            )}
-            />
+        <FlatList
+          numColumns={3}
+          columnWrapperStyle={estilos.flatlistColum}
+          data={dados}
+          renderItem={({ item }: { item: User }) => (
+            <UserCard nome={item.nome} foto={item.foto} />
+          )}
+        />
       </View>
-
     </View>
   );
 };
@@ -65,7 +59,7 @@ const estilos = StyleSheet.create({
   flatlistColum: {
     flex: 1,
     gap: 15,
-  }
+  },
 });
 
 export default SelecaoPerfil;
